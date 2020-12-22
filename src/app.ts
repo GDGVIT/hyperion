79a2e162006d4a0846a981608b7ddc55cf0946e3
abdealijaroli <abdealiburu03@gmail.com>
2020-12-22 21:48:24 +0530

    basic bot ready

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,3 @@
-/* const { Telegraf } = require('telegraf')
-const express = require('express')
-
-const bot = new Telegraf(process.env.TOKEN)
-// Set the bot response
-bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hack the World.</b>'))
-
-// Set telegram webhook
-// lt --port 3000
-bot.telegram.setWebhook('https://----.localtunnel.me/start')
-
-const app = express()
-app.get('/', (req, res) => res.send('Hack the World.'))
-// Set the bot API endpoint
-app.use(bot.webhookCallback('/start'))
-app.listen(3000, () => {
-  console.log('Listening on 3000...')
-}) */
 const { Telegraf } = require('telegraf')
 require('dotenv').config()
 
@@ -27,6 +9,4 @@ bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.hears('howdy', Telegraf.reply("i'm killin' it"))
 bot.hears('tell me a joke', ({ reply }) => reply('your life.'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-
-// launch bot
 bot.launch()
