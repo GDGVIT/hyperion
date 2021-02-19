56b667b822573fc3865e5dcde5ded90b8a296719
Anshul Bamb <anshulbamb22@gmail.com>
2021-02-19 13:09:23 +0530

    logging for commands + winston used

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { Telegraf } from 'telegraf'
+import logger from '../src/logging/logger'
 import schedule from 'node-schedule'
 import { allContests } from './api/kickStart/kickstart'
 import { upcomingContestsCodeforces, runningContestsCodeforces } from './api/codeforces/codeforces'
@@ -16,11 +17,26 @@ let bot
 if (process.env.TOKEN != null) {
   bot = new Telegraf(process.env.TOKEN)
 } else {
+  logger.error('Bot token invalid or null')
   process.exit(2)
 }
 
-bot.start((ctx) => ctx.reply(constants.startMessage))
-bot.help((ctx) => ctx.reply(constants.helpMessage))
+bot.start((ctx) => {
+  try {
+    ctx.reply(constants.startMessage)
+    logger.info('Bot started perfectly')
+  } catch (e) {
+    logger.error('Bot could not start\n:' + e)
+  }
+})
+bot.help((ctx) => {
+  try {
+    ctx.reply(constants.helpMessage)
+    logger.info('Help ran perfectly')
+  } catch (e) {
+    logger.error('Help was not succesful\n:' + e)
+  }
+})
 
 // RESULTS
 bot.hears('0', (ctx) => {
@@ -36,10 +52,16 @@ bot.command('cf_upcoming', async (ctx) => {
   for (const i of result.result) {
     s = s + '\n\n' + getCodeforcesString(i.name, i.id, i.startTimeSeconds)
   }
-  if (s === '') {
-    ctx.reply(constants.noContestMessage)
-  } else {
-    ctx.reply(constants.codeForcesReplyUpcoming + s, Extra.HTML())
+  try {
+    if (s === '') {
+      ctx.reply(constants.noContestMessage)
+      logger.info('Command (cf_upcoming) ran perfectly')
+    } else {
+      ctx.reply(constants.codeForcesReplyUpcoming + s, Extra.HTML())
+      logger.info('Command (cf_upcoming) ran perfectly')
+    }
+  } catch (e) {
+    logger.error('Command (cf_upcoming) not succesful\n:' + e)
   }
 })
 
@@ -49,10 +71,16 @@ bot.command('cf_running', async (ctx) => {
   for (const i of result.result) {
     s = s + '\n\n' + getCodeforcesString(i.name, i.id, i.startTimeSeconds)
   }
-  if (s === '') {
-    ctx.reply(constants.noContestMessage)
-  } else {
-    ctx.reply(constants.codeForcesReplyRunning + s, Extra.HTML())
+  try {
+    if (s === '') {
+      ctx.reply(constants.noContestMessage)
+      logger.info('Command (cf_running) ran perfectly')
+    } else {
+      ctx.reply(constants.codeForcesReplyRunning + s, Extra.HTML())
+      logger.info('Command (cf_running) ran perfectly')
+    }
+  } catch (e) {
+    logger.error('Command (cf_running) not succesful\n:' + e)
   }
 })
 
@@ -63,10 +91,16 @@ bot.command('cc_upcoming', async (ctx) => {
   for (const i of result.result) {
     s = s + '\n\n' + getCodeChefStringUpcoming(i.name, i.code, i.startDate)
   }
-  if (s === '') {
-    ctx.reply(constants.noContestMessage)
-  } else {
-    ctx.reply(constants.codeChefReplyUpcoming + s, Extra.HTML())
+  try {
+    if (s === '') {
+      ctx.reply(constants.noContestMessage)
+      logger.info('Command (cc_upcoming) ran perfectly')
+    } else {
+      ctx.reply(constants.codeChefReplyUpcoming + s, Extra.HTML())
+      logger.info('Command (cc_upcoming) ran perfectly')
+    }
+  } catch (e) {
+    logger.error('Command (cc_upcoming) not succesful\n:' + e)
   }
 })
 bot.command('cc_running', async (ctx) => {
@@ -75,10 +109,16 @@ bot.command('cc_running', async (ctx) => {
   for (const i of result.result) {
     s = s + '\n\n' + getCodeChefStringRunning(i.name, i.code, i.endDate)
   }
-  if (s === '') {
-    ctx.reply(constants.noContestMessage)
-  } else {
-    ctx.reply(constants.codeChefReplyRunning + s, Extra.HTML())
+  try {
+    if (s === '') {
+      ctx.reply(constants.noContestMessage)
+      logger.info('Command (cc_running) ran perfectly')
+    } else {
+      ctx.reply(constants.codeChefReplyRunning + s, Extra.HTML())
+      logger.info('Command (cc_running) ran perfectly')
+    }
+  } catch (e) {
+    logger.error('Command (cc_running) not succesful\n:' + e)
   }
 })
 
@@ -89,10 +129,16 @@ bot.command('ac_contests', async (ctx) => {
   for (const i of events.result) {
     s = s + '\n\n' + getAtcoderString(i.title, i.id, i.startTimeSeconds)
   }
-  if (s === '') {
-    ctx.reply(constants.noContestMessage)
-  } else {
-    ctx.reply(constants.atCoderReply + s, Extra.HTML())
+  try {
+    if (s === '') {
+      ctx.reply(constants.noContestMessage)
+      logger.info('Command (ac_contests) ran perfectly')
+    } else {
+      ctx.reply(constants.atCoderReply + s, Extra.HTML())
+      logger.info('Command (ac_contests) ran perfectly')
+    }
+  } catch (e) {
+    logger.error('Command (ac_contests) not succesful\n:' + e)
   }
 })
 
@@ -103,10 +149,16 @@ bot.command('ks_contests', async (ctx) => {
   for (const i of events) {
     s = s + '\n\n' + getKickStart(i.name, i.startDate, i.startTime)
   }
-  if (s === '') {
-    ctx.reply(constants.noContestMessage)
-  } else {
-    ctx.reply(constants.kickStartReply + s, Extra.HTML())
+  try {
+    if (s === '') {
+      ctx.reply(constants.noContestMessage)
+      logger.info('Command (ks_contests) ran perfectly')
+    } else {
+      ctx.reply(constants.kickStartReply + s, Extra.HTML())
+      logger.info('Command (ks_contests) ran perfectly')
+    }
+  } catch (e) {
+    logger.error('Command (ks_contests) not succesful\n:' + e)
   }
 })
 
@@ -125,10 +177,16 @@ bot.command('upcoming', async (ctx) => {
   for (const i of eventsAC.result) {
     resultString = resultString + '\n\n' + getAtcoderString(i.title, i.id, i.startTimeSeconds)
   }
-  if (resultString === '') {
-    ctx.reply(constants.noContestMessage)
-  } else {
-    ctx.reply(constants.miscReplyUpcoming + resultString, Extra.HTML())
+  try {
+    if (resultString === '') {
+      ctx.reply(constants.noContestMessage)
+      logger.info('Command (upcoming) ran perfectly')
+    } else {
+      ctx.reply(constants.miscReplyUpcoming + resultString, Extra.HTML())
+      logger.info('Command (upcoming) ran perfectly')
+    }
+  } catch (e) {
+    logger.error('Command (upcoming) not succesful\n:' + e)
   }
 })
 
@@ -142,15 +200,28 @@ bot.command('running', async (ctx) => {
   for (const i of eventsCC.result) {
     resultString = resultString + '\n\n' + getCodeChefStringRunning(i.name, i.code, i.endDate)
   }
-  if (resultString === '') {
-    ctx.reply(constants.noContestMessage)
-  } else {
-    ctx.reply(constants.miscReplyRunning + resultString, Extra.HTML())
+  try {
+    if (resultString === '') {
+      ctx.reply(constants.noContestMessage)
+      logger.info('Command (running) ran perfectly')
+    } else {
+      ctx.reply(constants.miscReplyRunning + resultString, Extra.HTML())
+      logger.info('Command (running) ran perfectly')
+    }
+  } catch (e) {
+    logger.error('Command (running) not succesful\n:' + e)
   }
 })
 
 // Editorial
-bot.command('sites', (ctx) => ctx.reply(constants.sitesMessage))
+bot.command('sites', (ctx) => {
+  try {
+    ctx.reply(constants.sitesMessage)
+    logger.info('Command (sites) ran perfectly')
+  } catch (e) {
+    logger.error('Command (sites) not succesful\n:' + e)
+  }
+})
 
 // scheduled messages
 bot.command('subscribe', async (ctx) => {
@@ -209,12 +280,40 @@ bot.command('subscribe', async (ctx) => {
 })
 
 // More about the community and devs
-bot.command('more', (ctx) => ctx.reply(constants.knowMore))
-bot.command('contribute', (ctx) => ctx.reply(constants.contributeMessage))
-bot.command('devs', (ctx) => ctx.reply(constants.devsMessage + '<a href="https://www.linkedin.com/in/anshul-bamb-b67830193">Anshul Bamb</a>\n\n<a href="https://abdealijaroli.tech">Abdeali Jaroli</a>\n\n<a href="https://mdhishaamakhtar.co">Md Hishaam Akhtar</a>\n\n<a href="https://www.linkedin.com/in/mayankk2/">Mayank Kumar</a>', Extra.HTML()))
-bot.command('community', (ctx) => ctx.reply(
-  constants.communityMessage + '<a href="https://dscvit.com/">Website</a>\n\n<a href="https://www.instagram.com/dscvitvellore/">Instagram</a>\n\n<a href="https://twitter.com/dscvit">Twitter</a>\n\n<a href="https://www.facebook.com/dscvitvellore">Facebook</a>\n\n<a href="https://www.linkedin.com/company/dscvit">Linkedin</a>\n\n<a href="https://medium.com/gdg-vit">Medium</a>\n\n<a href="https://www.youtube.com/channel/UCvT-ZJF7fXHJi9kDeCPR-zg">YouTube</a>', Extra.HTML()
-))
+bot.command('more', (ctx) => {
+  try {
+    ctx.reply(constants.knowMore)
+    logger.info('Command (more) ran perfectly')
+  } catch (e) {
+    logger.error('Command (more) not successful\n:' + e)
+  }
+})
+bot.command('contribute', (ctx) => {
+  try {
+    ctx.reply(constants.contributeMessage)
+    logger.info('Command (contribute) ran perfectly')
+  } catch (e) {
+    logger.error('Command (contribute) not successful\n:' + e)
+  }
+})
+bot.command('devs', (ctx) => {
+  try {
+    ctx.reply(constants.devsMessage + '<a href="https://www.linkedin.com/in/anshul-bamb-b67830193">Anshul Bamb</a>\n\n<a href="https://abdealijaroli.tech">Abdeali Jaroli</a>\n\n<a href="https://mdhishaamakhtar.co">Md Hishaam Akhtar</a>\n\n<a href="https://www.linkedin.com/in/mayankk2/">Mayank Kumar</a>', Extra.HTML())
+    logger.info('Command (devs) ran perfectly')
+  } catch (e) {
+    logger.error('Command (devs) not successful\n:' + e)
+  }
+})
+bot.command('community', (ctx) => {
+  try {
+    ctx.reply(
+      constants.communityMessage + '<a href="https://dscvit.com/">Website</a>\n\n<a href="https://www.instagram.com/dscvitvellore/">Instagram</a>\n\n<a href="https://twitter.com/dscvit">Twitter</a>\n\n<a href="https://www.facebook.com/dscvitvellore">Facebook</a>\n\n<a href="https://www.linkedin.com/company/dscvit">Linkedin</a>\n\n<a href="https://medium.com/gdg-vit">Medium</a>\n\n<a href="https://www.youtube.com/channel/UCvT-ZJF7fXHJi9kDeCPR-zg">YouTube</a>', Extra.HTML()
+    )
+    logger.info('Command (community) ran perfectly')
+  } catch (e) {
+    logger.error('Command (community) not successful\n:' + e)
+  }
+})
 
 // Redis
 export const client = redis.createClient(process.env.REDIS_URL)
@@ -222,7 +321,11 @@ export const redisSet = promisify(client.set).bind(client)
 export const redisGet = promisify(client.get).bind(client)
 
 client.on('connect', function () {
-  console.log('Redis Client Connected...')
+  try {
+    logger.info('Redis Client is connected')
+  } catch (e) {
+    logger.error('Unable to connect with the Redis Client')
+  }
 })
 
 // Launching the bot
--- src/db.ts
+++
@@ -1,5 +0,0 @@
-import lowdb from 'lowdb'
-import FileSync from 'lowdb/adapters/FileSync'
-
-const adapter = new FileSync('db.json')
-const db = lowdb(adapter)
